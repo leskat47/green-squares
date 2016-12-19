206920b43120a5ec334a00b440bd7eabc4a52ccf
Leslie Castellanos <lesliekate@gmail.com>
2016-12-18 18:37:35 -0800

    Organized code

--- githubscraper.py
+++ githubscraper.py
@@ -7,14 +7,17 @@ from github import get_longest_streak
 url_base = "https://github.com/"
 
 
-# participants = ["leskat47", "lobsterkatie", "jacquelineawatts", "franziskagoltz", "levi006", "allymcknight"]
-participants = ["jacquelineawatts"]
-for participant in participants:
-    url = requests.get(url_base + participant).text
-    soup = BeautifulSoup(url, 'html.parser')
-    squares = []
-    for square in soup.find_all('rect'):
-        if int(square["data-count"]) > 0:
-            squares.append(datetime.strptime(square['data-date'], "%Y-%m-%d").date())
-    # squares = [datetime.strptime(square['data-date'], "%Y-%m-%d").date() for square in soup.find_all('rect') if square['data-count'] > 0]
-    print participant, " ", get_longest_streak(squares)
+participants = ["leskat47", "lobsterkatie", "jacquelineawatts", "franziskagoltz", "levi006", "allymcknight"]
+
+def show_squares_data(participants):
+    for participant in participants:
+        url = requests.get(url_base + participant).text
+        soup = BeautifulSoup(url, 'html.parser')
+        squares = []
+        for square in soup.find_all('rect'):
+            if int(square["data-count"]) > 0:
+                squares.append(datetime.strptime(square['data-date'], "%Y-%m-%d").date())
+
+        print participant, " ", get_longest_streak(squares)
+
+show_squares_data(participants)
