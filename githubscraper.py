ee659ae406467d5fe5b7518cf1e85f9ed3eabc76
Leslie Castellanos <lesliekate@gmail.com>
2016-12-19 10:08:12 -0800

    Refactored code and made app email address

--- githubscraper.py
+++ githubscraper.py
@@ -6,60 +6,67 @@ from github import get_longest_streak
 from flask import Flask
 from flask_mail import Mail, Message
 
-app = Flask(__name__)
-app.config.update(
-    DEBUG=True,
-    #EMAIL SETTINGS
-    MAIL_SERVER='smtp.gmail.com',
-    MAIL_PORT=587,
-    MAIL_USE_SSL=False,
-    MAIL_USE_TLS=True,
-    MAIL_USERNAME = 'leslie@eagerorange.com',
-    MAIL_PASSWORD = os.environ['MAIL_PASSWORD']
-    )
 
-
-mail = Mail(app)
-
-url_base = "https://github.com/"
-participants = {"leskat47": {"name": "Leslie", "email": "leslie@eagerorange.com"},
-                "lobsterkatie": {"name": "Katie", "email": None},
-                "jacquelineawatts": {"name": "Jacqui", "email": None},
-                "franziskagoltz": {"name": "Franzi", "email": None},
-                "levi006": {"name": "Vi", "email": None},
-                "mcbishop": {"name": "Meg", "email": None},
-                "allymcknight": {"name": "Ally", "email": None}}
-
-
-def send_update_note(streak, participant, committed_today):
+def send_update_note(participant):
     """ Send status note and reminder to participants """
-    if participant["email"]:
-        subject = "Your Green Square Report"
-        if not committed_today:
-            message = "Hi %s! Don't forget to earn your green square for the day! You're currently at %d days of green squares." % (participant["name"], streak)
-        else:
-            message = "Hi %s! Good job earning your green square today! You're currently at %d days of green squares." % (participant["name"], streak)
-        with app.app_context():
-            msg = Message(subject, sender='leslie@eagerorange.com', recipients = [participant["email"]])
-            msg.body = message
-            mail.send(msg)
+
+    subject = "Your Green Square Report"
+    if not participant["committed_today"]:
+        message = "Hi %s! Don't forget to earn your green square for the day! You're currently at %d days of green squares." % (participant["name"], participant["streak"])
+    else:
+        message = "Hi %s! Good job earning your green square today! You're currently at %d days of green squares." % (participant["name"], participant["streak"])
+    with app.app_context():
+        msg = Message(subject, sender='greensquarechallenge@gmail.com', recipients = [participant["email"]])
+        msg.body = message
+        mail.send(msg)
 
     return
 
-def show_squares_data(participants):
+
+def get_date_squares(url_base, participant):
+    """ Return list of consecutive squares dates for a participant from scraped data """
+
+    url = requests.get(url_base + participant).text
+    soup = BeautifulSoup(url, 'html.parser')
+    squares = []
+    for square in soup.find_all('rect'):
+        if int(square["data-count"]) > 0:
+            squares.append(datetime.strptime(square['data-date'], "%Y-%m-%d").date())
+    return squares
+
+
+def update_participant_status():
+    """ Run status check and update user by email """
+
+    url_base = "https://github.com/"
+    participants = [{"id": "leskat47", "name": "Leslie", "email": "leslie@eagerorange.com"},
+                    {"id": "lobsterkatie", "name": "Katie", "email": None},
+                    {"id": "jacquelineawatts", "name": "Jacqui", "email": None},
+                    {"id": "franziskagoltz", "name": "Franzi", "email": None},
+                    {"id": "levi006", "name": "Vi", "email": None},
+                    {"id": "mcbishop", "name": "Meg", "email": None},
+                    {"id": "allymcknight", "name": "Ally", "email": None}]
     for participant in participants:
-        url = requests.get(url_base + participant).text
-        soup = BeautifulSoup(url, 'html.parser')
-        squares = []
-        for square in soup.find_all('rect'):
-            if int(square["data-count"]) > 0:
-                squares.append(datetime.strptime(square['data-date'], "%Y-%m-%d").date())
+        dates = get_date_squares(url_base, participant["id"])
+        participant["committed_today"] = (dates[-1] == date.today())
+        participant["streak"] = get_longest_streak(dates)
+        if participant["email"]:
+            send_update_note(participant)
 
-        streak = get_longest_streak(squares)
-        committed_today = (squares[-1] == date.today())
 
-        send_update_note(streak, participants[participant], committed_today)
+if __name__ == "__main__":
 
-        print participant, " ", streak
+    app = Flask(__name__)
+    app.config.update(
+        DEBUG=True,
+        #EMAIL SETTINGS
+        MAIL_SERVER='smtp.gmail.com',
+        MAIL_PORT=587,
+        MAIL_USE_SSL=False,
+        MAIL_USE_TLS=True,
+        MAIL_USERNAME = 'greensquarechallenge@gmail.com',
+        MAIL_PASSWORD = os.environ['MAIL_PASSWORD']
+        )
 
-show_squares_data(participants)
+    mail = Mail(app)
+    update_participant_status()
